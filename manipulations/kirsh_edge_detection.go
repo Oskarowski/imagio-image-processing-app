cec4bf5fcbdad35d4ff1976f246561db75fab1ee
Oskarowski <oscar.kacprzak@gmail.com>
2024-11-11 16:00:45 +0100

    handle color imgs via HSV

--- manipulations/kirsh_edge_detection.go
+++ manipulations/kirsh_edge_detection.go
@@ -10,7 +10,8 @@ import (
 func ApplyKirshEdgeDetection(img image.Image) *image.RGBA {
 	bounds := img.Bounds()
 	width, height := bounds.Max.X, bounds.Max.Y
-	edgeImage := image.NewGray(bounds)
+
+	edgeImage := image.NewRGBA(bounds)
 	draw.Draw(edgeImage, bounds, img, bounds.Min, draw.Src)
 
 	neighborhood := [][]int{
@@ -21,39 +22,38 @@ func ApplyKirshEdgeDetection(img image.Image) *image.RGBA {
 		for x := 1; x < width-1; x++ {
 
 			maxGradient := 0.0
-			neighborPixels := make([]int, 8)
+			neighborValues := make([]float64, 8)
 
 			for i := 0; i < 8; i++ {
 				dx, dy := neighborhood[i][0], neighborhood[i][1]
-				pixel := color.GrayModel.Convert(img.At(x+dx, y+dy)).(color.Gray)
-				neighborPixels[i] = int(pixel.Y)
+				pixel := color.RGBAModel.Convert(img.At(x+dx, y+dy)).(color.RGBA)
+				_, _, v := RGBToHSV(pixel.R, pixel.G, pixel.B)
+
+				neighborValues[i] = v
 			}
 
 			for i := 0; i < 8; i++ {
-				S := neighborPixels[i] +
-					neighborPixels[(i+1)%8] +
-					neighborPixels[(i+2)%8]
+				S := neighborValues[i] +
+					neighborValues[(i+1)%8] +
+					neighborValues[(i+2)%8]
 
-				T := neighborPixels[(i+3)%8] +
-					neighborPixels[(i+4)%8] +
-					neighborPixels[(i+5)%8] +
-					neighborPixels[(i+6)%8] +
-					neighborPixels[(i+7)%8]
+				T := neighborValues[(i+3)%8] +
+					neighborValues[(i+4)%8] +
+					neighborValues[(i+5)%8] +
+					neighborValues[(i+6)%8] +
+					neighborValues[(i+7)%8]
 
-				gradient := math.Abs(5*float64(S) - 3*float64(T))
+				gradient := math.Abs(5*S - 3*T)
 
 				if gradient > maxGradient {
 					maxGradient = gradient
 				}
 			}
 
-			edgeValue := uint8(math.Min(maxGradient, 255))
-			edgeImage.SetGray(x, y, color.Gray{Y: uint8(edgeValue)})
+			edgeValue := uint8(math.Min(maxGradient*255, 255))
+			edgeImage.SetRGBA(x, y, color.RGBA{R: edgeValue, G: edgeValue, B: edgeValue, A: 255})
 		}
 	}
 
-	rgbaImg := image.NewRGBA(bounds)
-	draw.Draw(rgbaImg, bounds, edgeImage, bounds.Min, draw.Src)
-
-	return rgbaImg
+	return edgeImage
 }
