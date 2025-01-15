15ae0e4833ecc1df2329cb10571cb1ff67e465ae
Oskarowski <oscar.kacprzak@gmail.com>
2025-01-15 22:48:14 +0100

    1 more test

--- orthogonal_transforms/fourier_test.go
+++ orthogonal_transforms/fourier_test.go
@@ -89,6 +89,53 @@ func TestHighPassFilterWithEdgeDetection2DImage2(t *testing.T) {
 	imageio.SaveBmpImage(ConvertComplexToImage(reconstructedImageMatrix), "test_F5test2_High_Pass_With_Edge_Detection_converted_image.bmp")
 	imageio.SaveBmpImage(ConvertFloatMatrixToImage(VisualizeSpectrum(reconstructedImageMatrix)), "test_F5test2_High_Pass_With_Edge_Detection_visualize_image.bmp")
 }
+func TestHighPassFilterWithEdgeDetection2DOnLena(t *testing.T) {
+	loadedImg, err := imageio.OpenBmpImage("../imgs/lenag.bmp")
+	// loadedImg, err := imageio.OpenBmpImage("../imgs/lenag_for_spectrum.bmp")
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+	complexImg := ConvertImageToComplex(loadedImg)
+
+	ftSpectrum := FFT2D(complexImg, false)
+
+	dcComponent := ftSpectrum[0][0]
+
+	shiftedSpectrum := QuadrantsSwap(ftSpectrum)
+
+	magnitude := FFTMagnitudeSpectrum(shiftedSpectrum)
+	normalized := NormalizeMagnitude(magnitude)
+	magnitudeImg := MagnitudeToImage(normalized)
+	imageio.SaveBmpImage(magnitudeImg, "test_lena_edge_detection_magnitude_spectrum.bmp")
+
+	maskImg, err := imageio.OpenBmpImage("./masks/F5mask5.bmp")
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+
+	binaryMask := morphological.ConvertIntoBinaryImage(maskImg)
+
+	filteredSpectrum := HighPassFilterWithEdgeDetection2D(shiftedSpectrum, binaryMask)
+
+	magnitudeAfterFilter := FFTMagnitudeSpectrum(filteredSpectrum)
+	normalizedAfterFilter := NormalizeMagnitude(magnitudeAfterFilter)
+	magnitudeImgAfterFilter := MagnitudeToImage(normalizedAfterFilter)
+	imageio.SaveBmpImage(magnitudeImgAfterFilter, "test_lena_edge_detection_after_filtered_magnitude_spectrum.bmp")
+
+	filteredSpectrum[0][0] = dcComponent
+
+	unshiftedFilteredSpectrum := QuadrantsSwap(filteredSpectrum)
+
+	magnitude2 := FFTMagnitudeSpectrum(unshiftedFilteredSpectrum)
+	normalized2 := NormalizeMagnitude(magnitude2)
+	magnitudeImg2 := MagnitudeToImage(normalized2)
+	imageio.SaveBmpImage(magnitudeImg2, "test_lena_edge_detection_unshifted_magnitude_spectrum.bmp")
+
+	reconstructedImageMatrix := FFT2D(unshiftedFilteredSpectrum, true)
+
+	imageio.SaveBmpImage(ConvertComplexToImage(reconstructedImageMatrix), "test_lena_edge_detection_converted_image.bmp")
+	imageio.SaveBmpImage(ConvertFloatMatrixToImage(VisualizeSpectrum(reconstructedImageMatrix)), "test_lena_edge_detection_visualize_image.bmp")
+}
 
 func TestBoatBandpass(t *testing.T) {
 	loadedImg, err := imageio.OpenBmpImage("../imgs/boat.bmp")
