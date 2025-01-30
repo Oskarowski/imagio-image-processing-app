54a957304413100a55251b2b6773138b015760e8
Oskarowski <oscar.kacprzak@gmail.com>
2025-01-30 15:23:54 +0100

    working files select and additional view to list selected files

--- cmd/tui/tui.go
+++ cmd/tui/tui.go
@@ -3,6 +3,7 @@ package tui
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 	"time"
@@ -11,11 +12,19 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+type view int
+
+const (
+	filePickerView view = iota
+	selectedFilesView
+)
+
 type model struct {
-	filepicker   filepicker.Model
-	selectedFile string
-	quitting     bool
-	err          error
+	filepicker    filepicker.Model
+	selectedFiles []string
+	currentView   view
+	quitting      bool
+	err           error
 }
 
 type clearErrorMsg struct{}
@@ -31,33 +40,39 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	m.filepicker, cmd = m.filepicker.Update(msg)
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
 			m.quitting = true
 			return m, tea.Quit
+
+		case "tab":
+			m.currentView = (m.currentView + 1) % 2
+			return m, nil
+
+		case "enter":
+			if m.currentView == filePickerView {
+				if didSelect, path := m.filepicker.DidSelectFile(msg); didSelect {
+					log.Default().Print("Selected file: ", path)
+					m.selectedFiles = append(m.selectedFiles, path)
+					return m, nil
+				}
+
+				if didSelect, path := m.filepicker.DidSelectDisabledFile(msg); didSelect {
+					log.Printf("Disabled file selected: %s", path)
+					m.err = errors.New(path + " is not valid.")
+					return m, clearErrorAfter(4 * time.Second)
+				}
+			}
+
 		}
+
 	case clearErrorMsg:
 		m.err = nil
-	}
-
-	var cmd tea.Cmd
-	m.filepicker, cmd = m.filepicker.Update(msg)
-
-	// Did the user select a file?
-	if didSelect, path := m.filepicker.DidSelectFile(msg); didSelect {
-		// Get the path of the selected file.
-		m.selectedFile = path
-	}
-
-	// Did the user select a disabled file?
-	// This is only necessary to display an error to the user.
-	if didSelect, path := m.filepicker.DidSelectDisabledFile(msg); didSelect {
-		// Let's clear the selectedFile and display an error.
-		m.err = errors.New(path + " is not valid.")
-		m.selectedFile = ""
-		return m, tea.Batch(cmd, clearErrorAfter(2*time.Second))
 	}
 
 	return m, cmd
@@ -67,16 +82,28 @@ func (m model) View() string {
 	if m.quitting {
 		return ""
 	}
+
 	var s strings.Builder
-	s.WriteString("\n  ")
-	if m.err != nil {
-		s.WriteString(m.filepicker.Styles.DisabledFile.Render(m.err.Error()))
-	} else if m.selectedFile == "" {
-		s.WriteString("Pick a file:")
-	} else {
-		s.WriteString("Selected file: " + m.filepicker.Styles.Selected.Render(m.selectedFile))
+
+	switch m.currentView {
+	case filePickerView:
+		s.WriteString("\n  Select files (Press 'Tab' to view selected files, 'Enter' to add):\n")
+		if m.err != nil {
+			s.WriteString("  " + m.filepicker.Styles.DisabledFile.Render(m.err.Error()) + "\n")
+		}
+		s.WriteString("\n" + m.filepicker.View() + "\n")
+
+	case selectedFilesView:
+		s.WriteString("\n  Selected files (Press 'Tab' to view file picker):\n")
+		if len(m.selectedFiles) == 0 {
+			s.WriteString("  No files selected.\n")
+		} else {
+			for i, f := range m.selectedFiles {
+				s.WriteString(fmt.Sprintf(" %d. ", i) + m.filepicker.Styles.Selected.Render(f) + "\n")
+			}
+		}
 	}
-	s.WriteString("\n\n" + m.filepicker.View() + "\n")
+
 	return s.String()
 }
 
@@ -89,9 +116,11 @@ func RunAsTUIApp() {
 	fp.CurrentDirectory, _ = os.Getwd()
 
 	m := model{
-		filepicker: fp,
+		filepicker:  fp,
+		currentView: filePickerView,
 	}
+
 	tm, _ := tea.NewProgram(&m).Run()
 	mm := tm.(model)
-	fmt.Println("\n  You selected: " + m.filepicker.Styles.Selected.Render(mm.selectedFile) + "\n")
+	fmt.Println("\nSee you next time " + mm.filepicker.Styles.Selected.Render("👋"))
 }
